8776fedb21b9e2ef530638d9bd03e8a0692251b2
Joseph Kain <joekain@gmail.com>
2013-05-13 22:33:26 -0700

    Add the Updater class.
    This class will handle all of the updates to the UI.  This allows
    the ViewController to have the single responsibility of handling
    the view's lifecycle.

---
+++ JKUpdaterExample/Updater.h
@@ -0,0 +1,13 @@
+//
+//  Updater.h
+//  JKUpdaterExample
+//
+//  Created by Joseph Kain on 5/13/13.
+//  Copyright (c) 2013 Joseph Kain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Updater : NSObject
+
+@end
---
+++ JKUpdaterExample/Updater.m
@@ -0,0 +1,13 @@
+//
+//  Updater.m
+//  JKUpdaterExample
+//
+//  Created by Joseph Kain on 5/13/13.
+//  Copyright (c) 2013 Joseph Kain. All rights reserved.
+//
+
+#import "Updater.h"
+
+@implementation Updater
+
+@end
