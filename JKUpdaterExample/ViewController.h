2f10102af9b0061390a330b939296e5425c892e7
Joseph Kain <joekain@gmail.com>
2013-05-13 22:06:18 -0700

    Initial Commit

---
+++ JKUpdaterExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  JKUpdaterExample
+//
+//  Created by Joseph Kain on 5/13/13.
+//  Copyright (c) 2013 Joseph Kain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
