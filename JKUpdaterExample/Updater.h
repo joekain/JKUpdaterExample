b40eaf088113c10f84d8c2dc9568008715731750
Joseph Kain <joekain@gmail.com>
2013-05-18 11:24:57 -0700

    Implement the Updater
    Add a set of methods to setup the updater with a new model.  The
    Updater will
    * Observe changes on the Model.
    * Populate the UI using values from the Model
    * Setup the indices used to refer to the buttons

--- JKUpdaterExample/Updater.h
+++ JKUpdaterExample/Updater.h
@@ -9,11 +9,21 @@
 
 #import <Foundation/Foundation.h>
 
+@class Model;
+
 @interface Updater : NSObject
 @property (weak, nonatomic) IBOutlet UILabel *labelTop;
 @property (weak, nonatomic) IBOutlet UILabel *labelBottom;
 
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *buttons;
 
+@property (weak, nonatomic) Model *model;
+
+
+// This method should be called to perform all setup for the updater.
+- (void)setupWithModel:(Model *)model;
+
+// This method will update the entire UI.
 - (void)update;
+
 @end
--- JKUpdaterExample/Updater.m
+++ JKUpdaterExample/Updater.m
@@ -8,10 +8,47 @@
 //
 
 #import "Updater.h"
-
+#import "Model.h"
 @implementation Updater
 
+#pragma mark - Setup
+
+- (void)setupObservationOnModel:(Model *)model {
+    // Observe all properties on the model.  This call doesn't request the change dictionary as update
+    // always sets the current state in the UI.
+    for (NSString *property in @[@"colorLabelTop", @"colorLabelBottom"]) {
+        [model addObserver:self forKeyPath:property options:0 context:nil];
+    }
+}
+
+- (void) setupIndexing:(NSArray *)buttons {
+    // Set tags on all the buttons.  It doesn't matter what order we do this in.  The tags
+    // are used as a consistent set of indices between this Updater and the ViewController.
+    int currentTag = 0;
+    for (UIButton *button in buttons) {
+        button.tag = currentTag;
+        ++currentTag;
+    }
+}
+
+- (void)setupWithModel:(Model *)model {
+    [self setupObservationOnModel:model];
+    [self setupIndexing:self.buttons];
+    self.model = model;
+}
+
+#pragma mark - Update
+
+// This method is called when one of the observed properties changes
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object
+                        change:(NSDictionary *)change context:(void *)context {
+    // Update the entire UI whenever anything changes.
+    [self update];
+}
+
+
 - (void)update {
-    self.labelTop.textColor = [UIColor greenColor];
+    self.labelTop.textColor = self.model.colorLabelTop;
+    self.labelBottom.textColor = self.model.colorLabelBottom;
 }
 @end
